1d99b090923a009a9093acb83b41d501f20149e2
Tatiana Londoño <tatalond@gmail.com>
2023-03-02 14:55:39 -0500

    Se agrega codigo para cerra el modal

--- prueba.py
+++ prueba.py
@@ -1,8 +1,14 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
 from selenium.webdriver.chrome.service import Service
 import time
+
 
 servicio = Service('driver/chromedriver')
 
@@ -11,33 +17,38 @@ driver = webdriver.Chrome(service=servicio)
 
 driver.get('https://www.linio.com.co/')
 
+
+
 driver.maximize_window()
 
-action = ActionChains(driver)
 
-# modal = driver.find_element(By.CSS_SELECTOR, "dy-lb-close").click()
-# time.sleep(3)
+
+action = ActionChains(driver)
+time.sleep(3)
+modal = driver.find_element(By.CSS_SELECTOR, "body > div.dy-modal-container.dy-act-overlay > div.dy-modal-wrapper > div > div.dy-lb-close").click()
+time.sleep(3)
 
 
 menu = driver.find_element(By.CSS_SELECTOR, ".icon-hamburger").click()
-
-time.sleep(3)
-
+time.sleep(1)
+ 
 categoria = driver.find_element(By.XPATH, "//div[@id=\'main-menu\']/nav/ul/li[5]/a/span[2]")
+time.sleep(1)
 
 action.move_to_element(categoria).perform()
-time.sleep(3)
+time.sleep(1)
 
 portatiles = driver.find_element(By.XPATH, "//div[@id=\'main-menu\']/div/div[5]/div/div/div/a[2]").click()
 
-driver.maximize_window()
-# time.sleep(5)
 
 
 
 
 
 """ time.sleep(3)
+driver.maximize_window()
+# time.sleep(5)
+
 
 categoria = driver.find_element(By.XPATH, '/html/body/header/nav/div/div[5]/nav/ul/li[5]/a')
 
